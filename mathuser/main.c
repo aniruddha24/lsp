c852d9605f5ac5c7a6ef7c65979c2012d77ed91a
aniruddha24 <aniruddhavanjari22@gmail.com>
2017-10-14 09:11:13 +0530

    INTIAL COMMIT math user

--- mathuser/main.c
+++ mathuser/main.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
-#include<libmath.h>
+#include"libmath.h"
 //#include"printresult.h"
 int main()
 {
 int i,j;
+int arr[5] ={1,2,3,4,5};
 int result=add(i,j);
+printf( ":%d\n",result);
+result=mean(arr,5);
 printf( ":%d",result);
 return 0;
 }
